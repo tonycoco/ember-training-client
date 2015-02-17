cb18ebe6db916718235afe256ff5d3462dc48948
Tony Coconate <me@tonycoconate.com>
2015-02-17 01:43:57 -0600

    Adding tests for helper and edit template

--- tests/unit/helpers/mail-to-test.js
+++ tests/unit/helpers/mail-to-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import {
   mailTo
 } from '../../../helpers/mail-to';
@@ -8,4 +9,14 @@ module('MailToHelper');
 test('it works', function(assert) {
   var result = mailTo('fake@example.com');
   assert.ok(result);
+});
+
+test('is a SafeString', function(assert) {
+  var result = mailTo('fake@example.com');
+  assert.ok(result instanceof Ember.Handlebars.SafeString);
+});
+
+test('creates a mailable link with the email', function(assert) {
+  var result = mailTo('fake@example.com');
+  assert.equal(result.toString(), '<a href="mailto:fake@example.com">fake@example.com</a>');
 });
